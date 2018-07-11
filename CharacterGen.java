0a3a300f670fda5a255dbd034f52cf66e2774928
Jet Ardeshna <jardeshna@gmail.com>
2018-07-11 13:52:16 +0530

    CharacterGeneration

---
+++ CharacterGen.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+class CharacterGen{
+    public static void main(String[] args) {
+        Random var = new Random();
+        for (int i = 0; i < Integer.parseInt(args[0]); i++) {
+            int a = var.nextInt(26);
+            char c = (char)(a + 'A');
+            if(c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
+                continue;
+            }else{
+                System.out.println("Character:["+i+"]:"+c);
+            }
+        }
+    }
+}     
