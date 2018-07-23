aeaeda9ad6bb7bedef3216d189a7a3099503d1f0
Jet Ardeshna <jardeshna@gmail.com>
2018-07-24 02:20:07 +0530

    Constr

---
+++ ProjectJava/src/Box.java
@@ -0,0 +1,15 @@
+public class Box{
+    int h,w,l;
+    Box(){
+        h=10;
+        w=10;
+        l=10;
+    }
+    void display(){
+        System.out.println("\nThe total volume of the box is: "+h*w*l);
+    }
+    public static void main(String[] args) {
+        Box obj1 = new Box();
+        obj1.display();
+    }
+}
---
+++ ProjectJava/src/Constr.java
@@ -0,0 +1,21 @@
+class Box{
+    int h,w,l;
+    Box(){
+        h=1;w=10;l=10;
+    }
+    Box(int a,int b,int c){
+        h=a;l=b;w=c;
+    }
+    void display(){
+        System.out.println("The total volume of the box is: "+h*l*w);
+    }
+}
+
+class Constr{
+    public static void main(String[] args) {
+        Box obj1 = new Box();
+        Box obj2 = new Box(25,10,15);
+        obj1.display();
+        obj2.display();
+    }
+}
