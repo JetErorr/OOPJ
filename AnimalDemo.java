a05cbddb36ba2053e5e984062138257221c8ee28
Jet Ardeshna <jardeshna@gmail.com>
2018-07-25 08:10:14 +0530

    Animal Transport

---
+++ AnimalDemo.java
@@ -0,0 +1,42 @@
+interface Transport{
+    void deliver();
+}
+abstract class Animal{
+abstract void show();
+}
+class Tiger extends Animal{
+    void show(){}
+}
+class Deer extends Animal{
+    void show(){}
+}
+class Camel extends Animal implements Transport{
+    void show(){
+        deliver();
+    }
+public void deliver(){
+    System.out.println("Camel is used as a Delivery Animal.");
+}
+}
+class Donkey extends Animal implements Transport{
+    void show(){
+        deliver();
+    }
+public void deliver(){
+    System.out.println("Donkey is used as a Delivery Animal.");
+}
+}
+class AnimalDemo{
+    public static void main(String[] args) {
+        Animal[] a = new Animal[4];
+        a[0] = new Tiger();
+        a[1] = new Camel();
+        a[2] = new Deer();
+        a[3] = new Donkey();
+        for (int i = 0; i < a.length; i++) {
+            if(a[i] instanceof Transport){
+                a[i].show();
+            }
+        }
+    }
+}
---
+++ VegetableDemo.java
@@ -0,0 +1,32 @@
+abstract class Vegetable{
+    String color;
+    abstract public String toString();
+}
+class Potato extends Vegetable{
+    public String toString(){
+        color="White";
+        return color;
+    }
+}
+class Tomato extends Vegetable{
+    public String toString(){
+        color="Red";
+        return color;
+    }
+}
+class Brinjal extends Vegetable{
+    public String toString(){
+        color="Purple";
+        return color;
+    }
+}
+class VegetableDemo{
+    public static void main(String []args){
+        Potato p = new Potato();
+        Brinjal b = new Brinjal();
+        Tomato t = new Tomato();
+        System.out.println("Color of Potato is: "+p.toString());
+        System.out.println("Color of Tomato is: "+t.toString());
+        System.out.println("Color of Brinjal is: "+b.toString());
+    }
+}
