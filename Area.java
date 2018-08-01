42134a7fb33e09674f79792e2c7410c42efbc8ef
Jet Ardeshna <jardeshna@gmail.com>
2018-08-01 12:15:15 +0530

    SplitString

---
+++ Area.java
@@ -0,0 +1,9 @@
+abstract class shape{
+    abstract void area();
+}
+class triangle extends shape{
+    float a=12,b=9,c=5;
+    void area(){
+        System.out.println("Area is: "+((a/2)*b);
+    }
+}
---
+++ ProjectJava/src/SplitString.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class SplitString{
+    public static void main(String[] args){
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter your string:\n");
+        String s = Sc.nextLine();
+        int n=s.length();
+        System.out.println("The length of the string is = "+ n);
+        System.out.print("\b\band the second half of the string is: ");
+        for(int i=n-1;i<n-4;i++){
+            System.out.print(s.charAt(i));
+        }
+        Sc.close();
+    }
+}
