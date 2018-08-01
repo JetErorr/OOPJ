758e5d97b8483f3d36bd12accfbb00ff256bc6ee
Jet Ardeshna <jardeshna@gmail.com>
2018-08-01 13:04:35 +0530

    5/3

--- Area.java
+++ Area.java
@@ -2,8 +2,30 @@ abstract class shape{
     abstract void area();
 }
 class triangle extends shape{
-    float a=12,b=9,c=5;
+    float a=12,c=5;
     void area(){
-        System.out.println("Area is: "+((a/2)*b);
+		System.out.println("Area is: "+((a/2)*c));
+    }
+}
+class rectangle extends shape{
+    float a=10,b=20;
+    void area(){
+        System.out.println("Area is: "+a*b);
+    }
+}
+class circle extends shape{
+    double r=2.5,pi=3.14;
+    void area(){
+        System.out.println("Area is: "+pi*(r*r));
+    }
+}
+class Area{
+    public static void main(String[] args) {
+        triangle t = new triangle();
+        rectangle r = new rectangle();
+        circle c = new circle();
+        t.area();
+        r.area();
+        c.area();
     }
 }
---
+++ Tutorial5/Exception3.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+class Exception3{
+     public static void main(String[] args) {
+        Scanner var = new Scanner(System.in);
+        
+     }
+}
