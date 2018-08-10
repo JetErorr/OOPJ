2a58443266d400b8c56e7213f28c9f67b0f64a14
jeterorr <jardeshna@gmail.com>
2018-08-10 11:25:34 +0530

    Recursion Power / Multiply

--- Tutorial4/ProjectJava/src/RecursionMultiply.java
+++ Tutorial4/ProjectJava/src/RecursionMultiply.java
@@ -1,10 +1,10 @@
-public class Recursion{
+public class RecursionMultiply{
     int power(int n, int m){
         if(m==1){return n;}
-        else{return n*power(n, m-1);}
+        else{return n+power(n, m-1);}
     }
     public static void main(String[] args) {
         Recursion obj = new Recursion();
-        System.out.println("5^3= "+obj.power(5, 3));
+        System.out.println("5*3= "+obj.power(5, 3));
     }
 }
