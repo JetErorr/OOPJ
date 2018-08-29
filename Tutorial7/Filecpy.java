9af71454db55a53fe843d1f6bd240151d931d153
Jet Ardeshna <jardeshna@gmail.com>
2018-08-29 12:49:27 +0530

    CopyFile

---
+++ Tutorial7/Filecpy.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+class FileCpy{
+    public static void main(String[] args) throws IOException {
+        // File f = new File(args[0]);
+        int i;
+        FileInputStream fin = new FileInputStream(args[0]);
+        BufferedInputStream bin = new BufferedInputStream(fin);
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(args[1]));
+        while((i=bin.read())!=-1){
+            bos.write((char)i);
+        }
+        System.out.println("File content copied.!");
+        bin.close();
+        bos.close();
+    }
+}
--- Tutorial7/ToLowerCse.java
+++ Tutorial7/ToLowerCse.java
@@ -1,9 +1,10 @@
+import java.io.*;
 class ToLowerCse {
     public static void main(String[] args) throws IOException {
         int i;
         char ch;
         FileReader fil = new FileReader(args[0]);
-        while ((i = fil.read()) != -1) {
+        while ((ch = fil.read()) != -1) {
             if (Character.isUpperCase(ch)) {
                 Character.toLowerCase(ch);
             }  System.out.print(ch);
