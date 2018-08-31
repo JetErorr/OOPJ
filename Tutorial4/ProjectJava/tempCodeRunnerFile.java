6f7daf704d6f2395d534010da483d7066a813ad0
Jet Ardeshna <jardeshna@gmail.com>
2018-08-31 11:27:28 +0530

    DemoStatic

---
+++ Tutorial4/ProjectJava/tempCodeRunnerFile.java
@@ -0,0 +1,4 @@
+    // public static void main(String[] args) {
+    //     System.out.println("This line is from the main block");
+    //     System.out.println("Even the main block will give way to the static block.!");
+    
