6b35b0240a1e7e30a3b5d90597c28c586f42320a
JetErorr <jardeshna@gmail.com>
2019-01-11 09:06:55 +0530

    .

--- multi.java
+++ multi.java
@@ -1,21 +1,19 @@
 class multi extends Thread {
     public void run()
-
     {
-        System.out.println("thread is creatin");
+        System.out.println("thread is created");
     }
 
-    public void stop();
-
-    {
-        System.out.println("thread is stop");
-    }
+    // public void stop()
+    // {
+    //     System.out.println("thread is stopped");
+    // }
 
     public static void main(String[] args) {
         multi m1 = new multi();
         // multi m2 = new multi();
         m1.start();
-        m1.end();
+        // m1.stop();
 
     }
 }
--- test.java
+++ test.java
@@ -3,7 +3,7 @@ import java.net.*;
 class Test {
     public static void main(String[] args) throws Exception {
         InetAddress address = InetAddress.getLocalHost();
-        System.out.println(192.168.91.16);
+        System.out.println("192.168.91.16");
         address = InetAddress.getByName("www.studytonight.com");
         System.out.println(address);
         InetAddress sw[] = InetAddress.getAllByName("www.google.com");
