ff61908ff0c3d478999eef2c2662eb5f5ae0066c
JetErorr <jardeshna@gmail.com>
2019-02-01 12:54:31 +0530

    minor changes

--- Tut-10/UDPServer1.java
+++ Tut-10/UDPServer1.java
@@ -1,4 +1,3 @@
-import java.io.IOException;
 import java.net.*;
 import java.util.Scanner;
 
@@ -11,15 +10,14 @@ class UDPServer1 {
             System.out.println("Please enter some text here.");
             String msg = scan.nextLine();
             myBuffer = msg.getBytes();
-            URL
-            DatagramPacket mypack = new DatagramPacket(myBuffer, myBuffer.length, InetAddress.getByName("10.80.2.2"),
-                    7785);
+            // URL
+            DatagramPacket mypack = new DatagramPacket(myBuffer, myBuffer.length, InetAddress.getByName("10.80.2.2"), 7785);
             mySocket.send(mypack);
             if (msg.equals("exit")) {
                 break;
             }
+            scan.close();
         }
         mySocket.close();
-        // scan.close();
     }
 }
